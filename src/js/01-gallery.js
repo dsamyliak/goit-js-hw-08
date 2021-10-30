dcd75d21901378adf5c9b9bb11ab30d10ba89337
dsamyliak <dsamyliak@gmail.com>
2021-10-30 20:23:19 +0300

    Add 01-gallery.js and update package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryDiv = document.querySelector(".gallery");
+
+
+const galleryItemHtml = galleryItems.map((galItem) => 
+    `<a class="gallery__item" href="${galItem.original}">
+  <img class="gallery__image" src="${galItem.preview}" alt="${galItem.description}" />
+</a>`).join("");
+
+// galleryDivHtml
+galleryDiv.insertAdjacentHTML("beforeend", galleryItemHtml);
+
+// Create LightBox 
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
+//
+
 console.log(galleryItems);
+
