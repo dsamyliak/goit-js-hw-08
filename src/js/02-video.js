a69eed215665dd39319d254d35988b82a47b355b
dsamyliak <dsamyliak@gmail.com>
2021-10-31 00:06:55 +0300

    Add Task 02 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+var throttle = require('lodash.throttle');
+var iframe = document.querySelector('iframe');
+var player = new Vimeo.Player(iframe);
+    
+const lastSavedTime = localStorage.getItem("videoplayer-current-time");
+console.log(lastSavedTime);
 
+player.setCurrentTime(lastSavedTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(function (timeObj) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(timeObj.seconds));
+    // console.log("timeUpdateFunc:", timeObj.seconds);  
+}, 1000));
