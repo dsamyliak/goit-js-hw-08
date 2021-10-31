763a7c8494d2dea653870834fdc85cb9a5b87382
dsamyliak <dsamyliak@gmail.com>
2021-10-31 17:19:46 +0200

    Add Task 03 finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+var throttle = require('lodash.throttle');
+const feedbackForm = document.querySelector(".feedback-form");
 
+const savedFormDataJSON = localStorage.getItem("feedback-form-state");
+const savedFormData = JSON.parse(savedFormDataJSON);
+
+if (savedFormData !== null) {
+    feedbackForm["email"].value = savedFormData.email;
+    feedbackForm["message"].value = savedFormData.message;
+};
+
+feedbackForm.addEventListener("input", throttle(event => {
+
+    const formData = { email: `${feedbackForm["email"].value}`, message: `${feedbackForm["message"].value}` };
+    // console.log(formData);
+    const formDataJSON = JSON.stringify(formData);
+    // console.log(formDataJSON);
+
+    localStorage.setItem("feedback-form-state", formDataJSON);
+
+}, 500));
+
+feedbackForm.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const formData = { email: `${feedbackForm["email"].value}`, message: `${feedbackForm["message"].value}` };
+    console.log(formData);
+
+    //remove data from localStorage and feedback-form
+    localStorage.removeItem("feedback-form-state");
+    feedbackForm["email"].value = "";
+    feedbackForm["message"].value = "";
+
+});
